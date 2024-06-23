e6cfe97765bc4adba87e96dde55f8e2983f081c2
nbanda1 <148505016+nbanda1@users.noreply.github.com>
2024-06-23 20:06:21 +0200

    popravljeni decoder i simonsays

--- decoder.py
+++ decoder.py
@@ -1,10 +1,11 @@
 from machine import Pin, Timer
 
 class Decoder:
-    clk_okinut = dt_okinut = smjer = False
-    brojac=0
-    greske=0
-    prikaziCifru=0
+    rjesenja = [17,73,29,31,7,20,47,97]
+    clk_okinut = dt_okinut = smjer = greska = solved = False
+    brojac = 0
+    greske = 0
+
     brojevi = {
         0: (0, 0, 0, 0, 0, 0, 1),
         1: (1, 0, 0, 1, 1, 1, 1),
@@ -18,56 +19,83 @@ class Decoder:
         9: (0, 0, 0, 0, 1, 0, 0)
     }
 
-    # pins: clk, dt, sw
-    def __init__(self, pins:list, segmenti, digits, rjesenje)
-        self.solved=False
-        self.clk = Pin(pins[0], Pin.IN) # 
-        self.dt = Pin(pins[1], Pin.IN) # decoder dijelovi
-        self.sw = Pin(pins[2], Pin.IN) # 
-        self.rjesenje=10 * rjesenje + 10
-        self.segmenti=[Pin(pin,Pin.OUT) for pin in segmenti]
-        self.digits = [Pin(pin,Pin.OUT) for pin in digits]
-        
-        self.prikazujBrojeve=Timer(-1)
-        self.prikazujBroj.init(period=20,mode=Timer.PERIODIC,callback=self.prikaziBroj)
+    """
+    pins prima 3 elementa [clk,dt,sw]
+    segmenti prima 7 elemenata 
+    digits 2 
+    seed 0-7
+    """
+    def __init__(self, pins: list, segmenti:list, digits:list, seed):
+        self.clk = Pin(pins[0], Pin.IN)
+        self.dt = Pin(pins[1], Pin.IN)
+        self.sw = Pin(pins[2], Pin.IN)
+        self.rjesenje = self.rjesenja[seed]
+        self.segmenti = [Pin(pin, Pin.OUT) for pin in segmenti]
+        self.digits = [Pin(pin, Pin.OUT) for pin in digits]
+
+        self.prikazujBrojeve = Timer(period=20, mode=Timer.PERIODIC, callback=self.prikaziBroj)
         
         self.clk.irq(trigger=Pin.IRQ_FALLING, handler=self.okinutCLK)
         self.dt.irq(trigger=Pin.IRQ_FALLING, handler=self.okinutDT)
         self.sw.irq(trigger=Pin.IRQ_FALLING, handler=self.potvrdi)
 
-    def okinutCLK(self,pin):
-        self.clk_okinut = True
-        if (self.dt_okinut):
-            if self.brojac != 99:
-                self.brojac=self.brojac+1
-            self.clk_okinut = self.dt_okinut = False
+    def okinutCLK(self, pin):
+        if not self.greska and not self.solved:        
+            self.clk_okinut = True
+            if self.dt_okinut:
+                if self.brojac > 0:
+                    self.brojac -= 1
+                self.clk_okinut = self.dt_okinut = False
     
-    def okinutDT(self,pin):
-        self.dt_okinut = True
-        if (self.clk_okinut):
-            if self.brojac != 0:
-                self.brojac=self.brojac-1
-            self.clk_okinut = self.dt_okinut = False
+    def okinutDT(self, pin):
+        if not self.greska and not self.solved:        
+            self.dt_okinut = True
+            if self.clk_okinut:
+                if self.brojac < 99:
+                    self.brojac += 1
+                self.clk_okinut = self.dt_okinut = False
     
-    def potvrdi(self,pin):
-        if self.brojac == self.rjesenje:
-            self.solved=True
-            print("svaka cast")
-        else: 
-            self.greske = self.greske+1
-
-#moguci problemi
-#dodaj prikaz kad potrefis
-    def prikaziBroj(self,pin):
-        cifre = [brojac%10,brojac/10]
+    def potvrdi(self, pin):
+        if not self.greska and not self.solved:        
+            if self.brojac == self.rjesenje:
+                self.solved = True
+                prikazujUspjeh=Timer(period=500,mode=Timer.PERIODIC, callback=self.prikaziUspjeh)
+            else:
+                self.greska=True
+                self.greske += 1
+                vratiNaNulu=Timer(period=100,mode=Timer.PERIODIC, callback=self.prikaziGresku)
+    
+    prikaziCifru = 0
+    def prikaziBroj(self, pin):
+        cifre = [self.brojac // 10, self.brojac % 10]
         if self.prikaziCifru == 0:
             self.digits[1].off()
             for segment, state in zip(self.segmenti, self.brojevi[cifre[0]]):
                 segment.value(state)
             self.digits[0].on()
         else:
-            self.digits[0].off
+            self.digits[0].off()
             for segment, state in zip(self.segmenti, self.brojevi[cifre[1]]):
                 segment.value(state)
             self.digits[1].on()
-        self.prikaziCifru=(self.prikaziCifru+1)%2
+        self.prikaziCifru = (self.prikaziCifru + 1) % 2
+
+
+    def prikaziGresku(self,pin):
+        if(self.brojac>0):
+            self.brojac-=1
+        else:
+            self.greska=False
+            pin.deinit()
+
+    i=0
+    def prikaziUspjeh(self,pin):
+        if self.i == 0 :
+            self.prikazujBrojeve.deinit()
+            for segment in self.segmenti:
+                segment.value(1)
+        else:
+            self.prikazujBrojeve.init(period=20, mode=Timer.PERIODIC, callback=self.prikaziBroj)
+        self.i = (self.i + 1) % 2
+    
+
--- simonsays.py
+++ simonsays.py
@@ -1,30 +1,32 @@
 from machine import Pin, Timer
 import time
+import random
+
+
 class SimonSays:
     #vrijeme izmedju paljenja ledova u sekvenci
     T = 1000
     #unijeti brojeve pinova
-    def __init__(self, button_pins:list, led_pins:list, code:list):
-        self.buttons = [Pin(pin, Pin.IN, Pin.PULL_UP) for pin in button_pins]
+    def __init__(self, button_pins:list, led_pins:list):
+        self.buttons = [Pin(pin, Pin.IN, Pin.PULL_DOWN) for pin in button_pins]
         self.leds = [Pin(pin, Pin.OUT) for pin in led_pins]
-        self.code = code
-        self.show_timer=Timer(-1)
+        
+        
+        self.code = [random.randint(0, len(led_pins)-1) for _ in range(5)]
         self.output_index=0
         self.input_index=0
         self.user_input=[]
+        self.showingSequence = False
         self.DEBOUNCE_TIME = 200
         self.last_interrupt_time = 0
         self.solved=False
         self.greske=0
         for button in self.buttons:
             button.irq(trigger=Pin.IRQ_RISING,handler=self.button_handler)
-        self.checkingInput=Timer(-1)
-        self.prikazujSekvencu=Timer(-1)
-        
-        self.checkingInput.init(period=200,mode=Timer.PERIODIC,callback=self.checkInput)
-        self.show_timer.init(period=self.T,mode=Timer.PERIODIC,callback=self.turn_on_led)#PRIKAZI SEKVENCU
+        self.checkingInput = Timer(period=200,mode=Timer.PERIODIC,callback=self.checkInput)
+        self.show_timer = Timer(period=self.T,mode=Timer.PERIODIC,callback=self.turn_on_led)#PRIKAZI SEKVENCU
         #vrijeme izmedju prikazivanja sekvence
-        self.prikazujSekvencu.init(period=self.T*len(self.code)*2,mode=Timer.PERIODIC,callback=self.prikaziSekvencu)
+        self.prikazujSekvencu = Timer(period=self.T*len(self.code)*2,mode=Timer.PERIODIC,callback=self.prikaziSekvencu)
 
     def prikaziSekvencu(self,pin):
         if  not self.user_input and not self.solved: 
@@ -34,7 +36,6 @@ class SimonSays:
     def checkInput(self,pin):
         if len(self.user_input)==len(self.code):    
             if  self.user_input == self.code:
-                print("Bravo!")
                 for led in self.leds:
                     led.on()
                 self.solved=True
@@ -43,26 +44,26 @@ class SimonSays:
                 self.output_index=0
                 self.input_index=0
                 self.user_input=[]
-                self.reset_leds()
-                print("Ponovo.")
+                for led in self.leds:
+                    led.off()
                 self.greske = self.greske + 1
+                prikazujGresku = Timer(period=250,mode = Timer.PERIODIC,callback = self.prikaziGresku)
         
     def button_handler(self,pin):
-        current_time = time.ticks_ms()
-        if time.ticks_diff(current_time, self.last_interrupt_time) > self.DEBOUNCE_TIME:
-            self.last_interrupt_time = current_time
-            if self.input_index < len(self.code):
-                for i, button in enumerate(self.buttons):
-                    if button is pin:
-                        self.user_input.append(i)
-                self.input_index += 1    
+        if not self.showingSequence :
+            current_time = time.ticks_ms()
+            if time.ticks_diff(current_time, self.last_interrupt_time) > self.DEBOUNCE_TIME:
+                self.last_interrupt_time = current_time
+                if self.input_index < len(self.code):
+                    for i, button in enumerate(self.buttons):
+                        if button is pin:
+                            self.user_input.append(i)
+                    self.input_index += 1    
 
-    def reset_leds(self):
-        for led in self.leds:
-            led.off()
     
     def turn_on_led(self, pin):
         if self.output_index == 0:
+            self.showingSequence=True
             self.leds[self.code[self.output_index]].on()
             self.output_index = self.output_index + 1
         elif self.output_index < len(self.code):
@@ -72,18 +73,20 @@ class SimonSays:
         else:
             self.leds[self.code[self.output_index-1]].off()
             self.output_index=0
+            self.showingSequence = False
             pin.deinit()
-
-    def ShowFailure(self,pin):
+    
+    prikazujeGresku=False
+    def prikaziGresku(self,pin):
         if self.output_index==4:
             self.output_index=0
-            self.reset_leds()
+            for led in self.leds:
+                led.off()
+            prikazujeGresku = False
             pin.deinit()
         else:
             self.output_index+=1
             for led in self.leds:
-                led.value(not led.value())
+                led.toggle()
                     
-                
-
-#game = SimonSays([0,1,2,3],[4,5,6,7],[0,1,2,3])
+            
